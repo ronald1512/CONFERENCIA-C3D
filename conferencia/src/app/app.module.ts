a6b300590bad9e23e69c9d82acb950363f2ee9f9
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2020-09-21 18:20:55 -0600

    Se agregó el editor Codemirror para la entrada y la salida.
    Referencia: https://www.npmjs.com/package/@ctrl/ngx-codemirror

--- conferencia/src/app/app.module.ts
+++ conferencia/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CodemirrorModule } from '@ctrl/ngx-codemirror';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CodemirrorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
