3c6bd450be11f9d3e3715b163be09beb06305db4
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2020-09-22 22:09:17 -0600

    Se agregó la lectura al archivo de entrada por defecto

--- conferencia/src/app/app.component.ts
+++ conferencia/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import NodoAST from '../../models/NodoAST/NodoAST';
 import KeepData from '../models/Analisis/KeepData';
+import { HttpClient } from "@angular/common/http";
 declare var require: any
 @Component({
   selector: 'app-root',
@@ -11,9 +12,18 @@ declare var require: any
 export class AppComponent implements OnInit {
   entrada;
   salida;
-  ngOnInit() {
+  constructor(private httpClient: HttpClient){
 
   }
+  ngOnInit() {
+    this.abrirArchivoPruebas();
+  }
+  abrirArchivoPruebas(){
+    this.httpClient.get("assets/pruebas/main_file.txt", { responseType: 'text' }).subscribe(data =>{
+      this.entrada=data;
+    })
+  }
+
   analizar(){
     var parser = require('../models/Analisis/grammar');
     var respuesta:NodoAST=parser.parse(this.entrada);  
